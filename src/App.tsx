8593d5a3ecf182da8833bd9e7a2a53926350895b
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-02 13:50:13 +0530

    add code

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="h-screen big-red">
+      <div className="h-[95vh]"></div>
+      <div className="">
+        <input type="text"></input>
+      </div>
 
     </div>
   )
